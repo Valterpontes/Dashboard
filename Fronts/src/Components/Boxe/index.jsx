ca9361c4d57de867b1be1f0028e2f27698630f3c
Valter Oliveira Pontes Neto <125382304+Valterpontes@users.noreply.github.com>
2024-11-21 08:33:27 -0300

    Delete Fronts directory

--- Fronts/src/Components/Boxe/index.jsx
+++
@@ -1,9 +0,0 @@
-import { StyledWrapper } from "./style.js";
-
-export const Button = ({title}) => {
-  return (
-    <StyledWrapper>
-      <button>{ title }</button>
-    </StyledWrapper>
-  );
-}
--- Fronts/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["src\Pages\Home\index.jsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
